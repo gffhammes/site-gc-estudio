a43d8504be845741a894f02bde3c30f03e8f4fc6
Guilherme Hammes <gffhammes@gmail.com>
2022-08-28 12:41:16 -0300

    feat: add content to footer

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,6 +1,8 @@
-import { Box, Container } from '@mui/material';
+import { Box, Container, Stack, Typography } from '@mui/material';
 import React from 'react';
 import { defaultSectionPadding } from '../../constants/defaultSectionPadding';
+import { Button } from './Button';
+import { Logo } from './Logo';
 
 export const Footer = () => {
 	return (
@@ -10,7 +12,22 @@ export const Footer = () => {
 				padding: defaultSectionPadding,
 			}}
 		>
-			<Container></Container>
+			<Container>
+				<Stack>
+					<Logo />
+					<Box>
+						<Typography>Entre em contato</Typography>
+						<Typography>+55 (47) 99747-6478</Typography>
+						<Typography>contato@gcestudio.com.br</Typography>
+					</Box>
+					<Box>
+						<Typography>Quer evoluir sua marca? </Typography>
+						<Button color="secondary" variant="contained">
+							Iniciar um projeto
+						</Button>
+					</Box>
+				</Stack>
+			</Container>
 		</Box>
 	);
 };
