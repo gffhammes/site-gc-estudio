c1cb7368d06f5a3d3738f1b3265a2832e395e3eb
Guilherme Hammes <gffhammes@gmail.com>
2022-09-29 13:04:10 -0300

    refactor: delete contact form

--- src/components/ContactSection/ContactSection.tsx
+++ src/components/ContactSection/ContactSection.tsx
@@ -1,58 +1,71 @@
-import { Box, Container, Stack } from '@mui/material';
-import Image from 'next/image';
-import React from 'react';
-import { defaultSectionPadding } from '../../constants/defaultSectionPadding';
-import { useFetch } from '../../hooks/useFetch';
-import { MainText } from '../common/MainText';
-import { Section } from '../common/Section';
-import { ContactForm } from './ContactForm';
+import { Box, Stack, Typography } from "@mui/material";
+import Image from "next/image";
+import React from "react";
+import { useFetch } from "../../hooks/useFetch";
+import { Button } from "../common/Button";
+import { MainText } from "../common/MainText";
+import { Section } from "../common/Section";
+import WhatsAppIcon from "@mui/icons-material/WhatsApp";
 
 export const ContactSection = () => {
-	const { data, fetching } = useFetch<any>('/contato', {
-		params: {
-			populate: ['foto'],
-		},
-	});
+  const { data, fetching } = useFetch<any>("/contato", {
+    params: {
+      populate: ["foto"],
+    },
+  });
 
-	return (
-		<Section id="contato" backgroundColor="secondary.main">
-			<Stack
-				direction={{ xs: 'column', md: 'row' }}
-				spacing={10}
-				alignItems="center"
-				justifyContent="space-between"
-			>
-				<Box
-					sx={{
-						flexBasis: '50%',
-					}}
-				>
-					<MainText
-						title={data?.h2}
-						color="white.main"
-						dashPosition="top"
-						marginBottom
-					/>
+  return (
+    <Section id="contato" backgroundColor="secondary.main">
+      <Stack
+        direction={{ xs: "column", sm: "row" }}
+        spacing={10}
+        alignItems="center"
+        justifyContent="space-between"
+      >
+        <Box
+          sx={{
+            flexBasis: "50%",
+          }}
+        >
+          <MainText
+            title={data?.h2}
+            color="white.main"
+            dashPosition="top"
+            marginBottom
+          />
 
-					<ContactForm />
-				</Box>
+          <Stack spacing={4}>
+            <Typography color="#fff">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
+              mauris quam, dapibus et iaculis ac, aliquet ac erat.
+            </Typography>
 
-				<Box
-					sx={{
-						position: 'relative',
-						width: '100%',
-						maxWidth: { xs: '27rem', md: 'unset' },
-						aspectRatio: '1 / 1',
-						flexBasis: '50%',
-					}}
-				>
-					<Image
-						src={data?.foto.data.attributes.url || '/'}
-						alt="GC Estúdio"
-						layout="fill"
-					/>
-				</Box>
-			</Stack>
-		</Section>
-	);
+            <Button
+              color="primary"
+              variant="contained"
+              startIcon={<WhatsAppIcon />}
+            >
+              Fale conosco
+            </Button>
+          </Stack>
+        </Box>
+
+        <Box
+          sx={{
+            position: "relative",
+            width: "100%",
+            maxWidth: { xs: "27rem", md: "unset" },
+            aspectRatio: "1 / 1",
+            flexBasis: "50%",
+          }}
+        >
+          <Image
+            src={data?.foto.data.attributes.url || "/"}
+            alt="GC Estúdio"
+            layout="fill"
+          />
+        </Box>
+      </Stack>
+    </Section>
+  );
 };
--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,21 +1,18 @@
-import { Button as MuiButton, ButtonProps, styled } from '@mui/material';
-import React, { ReactNode } from 'react';
+import { Button as MuiButton, ButtonProps, styled } from "@mui/material";
+import React, { ReactNode } from "react";
 
 interface IButtonProps extends ButtonProps {
-	children: ReactNode;
+  children: ReactNode;
 }
 
-const StyledButton = styled(
-	MuiButton,
-	{}
-)(({ color }) => ({
-	width: 'fit-content',
-	m: 'auto',
-	fontWeight: 'bold',
-	textTransform: 'none',
-	color: color === 'white' ? '#ED2F3C' : 'white',
+const StyledButton = styled(MuiButton)(({ color }) => ({
+  width: "fit-content",
+  m: "auto",
+  fontWeight: "bold",
+  textTransform: "none",
+  color: color === "white" ? "#ED2F3C" : "white",
 }));
 
 export const Button = ({ children, ...props }: IButtonProps) => {
-	return <StyledButton {...props}>{children}</StyledButton>;
+  return <StyledButton {...props}>{children}</StyledButton>;
 };
