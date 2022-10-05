69c17c25d4c362635515f86e2eab94842447a6e4
Guilherme Hammes <gffhammes@gmail.com>
2022-10-05 19:47:12 -0300

    feat: add clients logos

--- src/components/ClientsSection/ClientsSection.tsx
+++ src/components/ClientsSection/ClientsSection.tsx
@@ -1,33 +1,49 @@
-import { Box, Container, Grid } from '@mui/material';
-import Image from 'next/image';
-import React from 'react';
-import { defaultSectionPadding } from '../../constants/defaultSectionPadding';
-import { useFetch } from '../../hooks/useFetch';
-import { MainText } from '../common/MainText';
-import { Section } from '../common/Section';
+import { Box, Container, Grid } from "@mui/material";
+import Image from "next/image";
+import React from "react";
+import { defaultSectionPadding } from "../../constants/defaultSectionPadding";
+import { useFetch } from "../../hooks/useFetch";
+import { MainText } from "../common/MainText";
+import { Section } from "../common/Section";
 
 export const ClientsSection = () => {
-	const { data, fetching } = useFetch<any>('/clientes', {
-		params: {
-			populate: ['clientes', 'clientes.logo'],
-		},
-	});
+  const { data, fetching } = useFetch<any>("/clientes", {
+    params: {
+      populate: ["clientes", "clientes.logo"],
+    },
+  });
 
-	return (
-		<Section id="clientes">
-			<MainText
-				title={data?.h2}
-				text={data?.textoPrincipal}
-				align="center"
-			/>
-			<Grid container columns={10}>
-				{data?.clientes.map((client: any, index: number) => (
-					<Grid key={index} item xs={5} sm={2}>
-						<Box>{/* <Image  /> */}</Box>
-						<Box>{client.nome}</Box>
-					</Grid>
-				))}
-			</Grid>
-		</Section>
-	);
+  return (
+    <Section id="clientes">
+      <MainText title={data?.h2} text={data?.textoPrincipal} align="center" />
+      <Box sx={{ mt: 5 }}>
+        <Grid
+          container
+          columns={10}
+          spacing={5}
+          alignItems="center"
+          justifyContent="center"
+        >
+          {data?.clientes.map((client: any, index: number) => (
+            <Grid key={index} item xs={5} sm={3} md={2}>
+              <Box sx={{ position: "relative", height: "6rem", width: "100%" }}>
+                <Box
+                  component={Image}
+                  src={client.logo.data.attributes.url}
+                  alt={client.nome}
+                  layout="fill"
+                  objectFit="contain"
+                  sx={{
+                    filter: "saturate(0)",
+                    transition: ".2s ease all",
+                    "&:hover": { filter: "saturate(1)" },
+                  }}
+                />
+              </Box>
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
+    </Section>
+  );
 };
